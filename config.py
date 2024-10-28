2d15e09700b326ac9ce728829e6d3ab97d086113
Anton Angelo <antonangelo@users.noreply.github.com>
2024-10-29 11:55:05 +1300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-repository_url = 'https://ir.canterbury.ac.nz/server/api'
-username = 'anton.angelo@canterbury.ac.nz'
-password = '5jnUiRm9hrsWYnW'
-
-google_api_key = "AIzaSyD8Dq_zW6FxADuSoB1tdo80Ee9qayF0XEM"
